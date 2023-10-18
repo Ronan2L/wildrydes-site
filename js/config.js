127a306908d350fb0a2d11b25d4db9e44b739b99
R2L <rlozachliber@aol.fr>
2023-10-18 14:15:35 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. eu-north-1_DEwpt34v4
+        userPoolClientId: '', // e.g. 15has9i8g35mfttla0ksuue4uv
+        region: '' // e.g. eu-north-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
